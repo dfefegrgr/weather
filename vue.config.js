3d6af21d8be85050a92061707ae33638e9e92d5b
dfefegrgr <jiade@jiade.com>
2022-05-14 21:19:04 +0800

    fix: 修复打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    assetsDir:'static',
+    parallel:false,
+    publicPath:'./',
+    runtimeCompiler: true,
+    transpileDependencies: true,
 })
